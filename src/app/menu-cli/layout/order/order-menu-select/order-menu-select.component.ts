36c352261a82e16ece0edeb4fddac425480e774e
Kamil Damian <kamil@MacBook-Pro-Kamil.local>
2022-08-12 21:35:18 +0200

    Update order selects

--- src/app/menu-cli/layout/order/order-menu-select/order-menu-select.component.ts
+++ src/app/menu-cli/layout/order/order-menu-select/order-menu-select.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { MenuItemSelectCollectionGet, OrderElementData } from 'src/app/openapi-cli/models';
+import { MenuItemSelectCollectionGet, MenuItemSelectGet, OrderElementData } from 'src/app/openapi-cli/models';
 
 @Component({
   selector: 'app-order-menu-select',
@@ -8,19 +8,22 @@ import { MenuItemSelectCollectionGet, OrderElementData } from 'src/app/openapi-c
 })
 export class OrderMenuSelectComponent implements OnInit {
 
-  _collection: MenuItemSelectCollectionGet | undefined;
-  @Input() set collection(value: MenuItemSelectCollectionGet) {
-    this._collection = value;
-  }
-
-  _order: OrderElementData | undefined;
-  @Input() set order(value: OrderElementData | undefined) {
-    this._order = value;
-  }
+  @Input('collection') collection!: MenuItemSelectCollectionGet;
+  @Input('order') order!: OrderElementData;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  change(select: MenuItemSelectGet, selected: boolean) {
+    var collectionSelects: string[] = this.collection.selects.map(e => e.ref);
+    this.order.selects = this.order.selects.filter(e => collectionSelects.indexOf(e.ref) === -1)
+
+    this.order.selects.push(select);
+    
+    console.log("Selects updated");
+    console.log(this.order);
   }
 
 }
