1ae01f64763da04af3e6eb1c69f0d91e840c062d
Kamil Damian <kamil@MacBook-Pro-Kamil.local>
2022-12-29 12:18:16 +0100

    First test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,8 @@
 describe('My First Test', () => {
   it('Visits the initial project page', () => {
-    cy.visit('/')
-    cy.contains('app is running!')
+    cy.visit('/menu/R00000000000/T00000000000')
+    // cy.get('#menu-elements app-menu-horizontal-element-wrapper h2').first().contains('Pizza Klasyka Klasyki!')
+
+    cy.get('#menu-elements app-menu-horizontal-element-wrapper h2').first().contains('Pizza Klasyka Klasyki');
   })
 })
